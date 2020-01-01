b63268f9eff3b93028553f49679abadba9a33320
Froilan Irizarry <froi@github.com>
2020-01-01 12:11:49 -0500

    Fixed lint error

--- examples/index.ts
+++ examples/index.ts
@@ -1,17 +1,16 @@
-import { Repo } from '../index';
-import fetch from 'node-fetch';
-import { Response } from 'node-fetch';
-
+import { Repo } from "../index";
+import fetch from "node-fetch";
+import { Response } from "node-fetch";
 
 function checkRepo(repo: Repo): boolean {
-  return repo.full_name === 'froi/github-types';
+  return repo.full_name === "froi/github-types";
 }
-fetch('https://api.github.com/repos/froi/github-types')
+fetch("https://api.github.com/repos/froi/github-types")
   .then((res: Response) => res.json())
   .then((repo: Repo) => {
-    if(checkRepo(repo)) {
-      console.log('All good.')
+    if (checkRepo(repo)) {
+      console.log("All good.");
     } else {
-      console.error('All bad');
+      console.error("All bad");
     }
   });
